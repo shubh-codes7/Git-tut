50d2c24c9eec816ef6396c3b3d19981ca2e92922
shubh-codes7 <shubhamcodes7@gmail.com>
2025-09-27 21:07:35 +0530

    second commit

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 const me = ["powerful", "rich"]
+
+for(let trait of me){
+  console.log(trait);
+}
+
+console.log("Done");
+
+const success = ["connections", "network", "access"]
+console.log("Am i doing enough?");
+console.log("Or am i enough?");
